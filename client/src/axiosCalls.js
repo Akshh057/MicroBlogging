8608f73a06e3c8932255148d30450eb1c45b5588
akshh057 <akshh057@gmail.com>
2021-09-04 20:53:47 +0530

    feat:changed the server url from localhost to heroku url

--- client/src/axiosCalls.js
+++ client/src/axiosCalls.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseUrl = 'https://mircoblogger.herokuapp.com'
+const baseUrl = 'https://mircoblogger.herokuapp.com/api'
 // All the axios request are performed over here, it is an object with key value pair, keys being the name of the request and values 
 // being the function.
 const API = {
     getAllBlogs: function () {
-        return axios.get(`${baseUrl}//getAllBlogs`)
+        return axios.get(`${baseUrl}/getAllBlogs`)
     },
     addBlog: function (newData) {
         return axios.post(`${baseUrl}/addBlog`, newData)
